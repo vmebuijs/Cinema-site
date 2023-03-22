716ff22caee81552e633d45de190591a229f7798
vmebuijs <valerie.buijs92@gmail.com>
2023-03-22 16:11:13 +0100

    first try home screen

---
+++ main.js
@@ -0,0 +1,6 @@
+#!/usr/bin nodejsvar 
+http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World.');
+}).listen(PORTNUMBER,'localhost');
