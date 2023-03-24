5b10d096adc2c003e9498fac3bc5adbd16299ece
vmebuijs <valerie.buijs92@gmail.com>
2023-03-24 17:16:19 +0100

    name change

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,14 @@
 var open = document.getElementsByClassName("menu-open")[0];
 var close = document.getElementsByClassName("menu-close")[0];
 
-var wrapper = document.getElementsByClassName("menu-container")[0].classList;
+var container = document.getElementsByClassName("menu-container")[0].classList;
 
 open.addEventListener("click", () => {
-  wrapper.toggle("opened");
-  wrapper.toggle("closed");
+  container.toggle("opened");
+  container.toggle("closed");
 });
 
 close.addEventListener("click", () => {
-  wrapper.toggle("opened");
-  wrapper.toggle("closed");
+  container.toggle("opened");
+  container.toggle("closed");
 });
