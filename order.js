5e066ba98abe32ba08fa70c967ff7bc9eb94e777
Hanna <h.s.mock@students.uu.nl>
2023-04-08 15:33:50 +0200

    start selecteren movie geeft date, werkt deels

--- order.js
+++ order.js
@@ -5,13 +5,74 @@ var movies = document.getElementById("movielist");
 var movielist = ["movie1","movie2","movie3","movie4"];
 movieID = [1,2,3,4];
 
-var movielist2 = [["movie1",1,"1 augustus"],["movie2",2,"2 augustus"],["movie3",3,"3 augustus"],["movie4",4,"4 augustus"]]
+var movielist2 = [["movie1",1,["1 augustus"]],["movie2",2,["2 augustus"]],["movie3",3,["3 augustus"]],["movie4",4,["4 augustus"]]]
 
-for(var i = 0; i < movielist.length; i++){
+for(var i = 0; i < movielist2.length; i++){
     var option = document.createElement("option");
     option.text = movielist2[i][0];
     option.value = movielist2[i][1];
     movies.add(option);
+}
+
+// movies.addEventListener("click", function() {
+//     var options = movies.querySelectorAll("option");
+//     var count = options.length;
+//     if(typeof(count) === "undefined" || count < 2)
+//     {
+//         addMovieItem();
+//     }
+// });
+
+movies.addEventListener("change", function() {
+        showMovieDates();
+});
+
+function showMovieDates() {
+
+    var movies = document.getElementById("movielist");
+    var selectedMovie = movies.options[movies.selectedIndex].value;
+    console.log(selectedMovie);
+
+    // als dropdown al bestaat: clear, anders:maak
+    if(document.getElementById("datelist") == null){
+        var location = document.getElementsByClassName("ordering-container")[0];
+        var dates = document.createElement("div");
+        var datess = document.createElement("select");
+        //var option1 = document.createElement("option");
+        
+        dates.setAttribute("class","dates");
+        datess.setAttribute("name","datelist");
+        datess.setAttribute("id","datelist");
+        addOptions(selectedMovie,datess);
+        console.log("niet bestaand");
+    }
+    else{
+        removeOptions(document.getElementById("datelist"));
+        addOptions(selectedMovie,datess);
+        console.log("bestaand");
+    }
+
+
+    dates.appendChild(datess);
+    location.appendChild(dates);
+    console.log("hallo");
+
+}
+
+function removeOptions(dropdownlist) {
+    var i, list = dropdownlist.options.length - 1;
+    for(i = list; i >= 0; i--){
+        dropdownlist.remove(i);
+    }
+}
+
+function addOptions(selectedMovie,datess) {
+    for(var i = 0; i < movielist2[selectedMovie][2].length; i++){
+        var option = document.createElement("option");
+        option.text = movielist2[selectedMovie][2][i];
+        option.value = movielist2[selectedMovie][1];
+        datess.add(option);
+    }
 }
 
 // function onChange(args) {
@@ -25,7 +86,6 @@ for(var i = 0; i < movielist.length; i++){
 //     }
 //     plekje.add(text);
 // };
-
 
 
 // fetch('http://localhost:8026/m')
