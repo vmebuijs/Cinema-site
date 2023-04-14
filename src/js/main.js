cef78c6bb9e93bc9f957e901dae379f3381adf8b
vmebuijs <valerie.buijs92@gmail.com>
2023-04-14 22:03:13 +0100

    commit before merge with server version

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin nodejsvar 
-http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World.');
-}).listen(PORTNUMBER,'localhost');
-
